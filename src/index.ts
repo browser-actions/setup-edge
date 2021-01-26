52cbe8105d01af9a07e15b1a309909a34f3a2b55
Shin'ya Ueoka <ueokande@i-beam.org>
2021-01-27 00:35:51 +0900

    fix query

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ async function run(): Promise<void> {
     await exec.exec("wmic", [
       "datafile",
       "where",
-      `name=${msedgeBin.replace(/\\/g, "\\\\")}`,
+      `name="${msedgeBin.replace(/\\/g, "\\\\")}"`,
       "get",
       "version",
     ]);

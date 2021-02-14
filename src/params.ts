ab123795ff5cd30f6cb8be3f611175466c3a7cdd
Shin'ya Ueoka <ueokande@i-beam.org>
2021-02-14 11:32:15 +0900

    Download edge installer from Edge insider page

--- src/params.ts
+++ src/params.ts
@@ -1,17 +1,20 @@
 export const StableVersion = "stable" as const;
 export const BetaVersion = "beta" as const;
 export const DevVersion = "dev" as const;
+export const CanaryVersion = "canary" as const;
 
 export type Version =
   | typeof StableVersion
   | typeof BetaVersion
-  | typeof DevVersion;
+  | typeof DevVersion
+  | typeof CanaryVersion;
 
 export const valueOfVersion = (value: string): Version => {
   switch (value) {
     case StableVersion:
     case BetaVersion:
     case DevVersion:
+    case CanaryVersion:
       return value;
     default:
       throw new Error("Unsupported version: " + value);

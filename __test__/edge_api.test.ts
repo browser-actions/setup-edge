720431753d297d889c38e28054c82b33d88620e6
Shin'ya Ueoka <ueokande@i-beam.org>
2024-05-04 15:00:00 +0900

    migrate tests to vitest

--- __test__/edge_api.test.ts
+++ __test__/edge_api.test.ts
@@ -1,5 +1,6 @@
 import * as fs from "node:fs";
 import * as path from "node:path";
+import { beforeEach, describe, expect, it } from "vitest";
 import { EdgeUpdates } from "../src/edge_api";
 
 describe("EdgeUpdates", () => {
